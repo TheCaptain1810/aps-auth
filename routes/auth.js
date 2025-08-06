e54356bebd564326934ddff356ba3e104bf83350
Pranav Pise <itspranavpise@gmail.com>
2025-08-06 14:34:39 +0530

    �

--- routes/auth.js
+++ routes/auth.js
@@ -7,36 +7,30 @@ const {
   getUserProfile,
 } = require("../services/aps");
 
-router.get("/api/auth/login", (req, res) => {
+router.get("/auth/login", (req, res) => {
   res.redirect(getAuthorizationUrl());
 });
 
-router.get("/api/auth/logout", (req, res) => {
+router.get("/auth/logout", (req, res) => {
   req.session = null;
   res.redirect("/");
 });
 
-router.get("/api/auth/callback", authCallbackMiddleware, (req, res) => {
+router.get("/auth/callback", authCallbackMiddleware, (req, res) => {
   res.redirect("/");
 });
 
-router.get("/api/auth/token", authRefreshMiddleware, (req, res) => {
+router.get("/auth/token", authRefreshMiddleware, (req, res) => {
   res.json(req.publicOAuthToken);
 });
 
-router.get(
-  "/api/auth/profile",
-  authRefreshMiddleware,
-  async (req, res, next) => {
-    try {
-      const userProfile = await getUserProfile(
-        req.publicOAuthToken.access_token
-      );
-      res.json(userProfile);
-    } catch (error) {
-      next(error);
-    }
+router.get("/auth/profile", authRefreshMiddleware, async (req, res, next) => {
+  try {
+    const userProfile = await getUserProfile(req.publicOAuthToken.access_token);
+    res.json(userProfile);
+  } catch (error) {
+    next(error);
   }
-);
+});
 
 module.exports = router;
