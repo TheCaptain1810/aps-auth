5b6689cf6c856b5df8211b6ed80e66102fc9198d
Pranav Pise <itspranavpise@gmail.com>
2025-08-06 17:03:12 +0530

    fixed the cjs error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import session from "cookie-session";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
 dotenv.config();
 
 const { SERVER_SESSION_SECRET } = process.env;
@@ -13,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   session({ secret: SERVER_SESSION_SECRET, maxAge: 24 * 60 * 60 * 1000 })
 );
-app.use(require("./routes/auth.js"));
+app.use(authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Express server!");
